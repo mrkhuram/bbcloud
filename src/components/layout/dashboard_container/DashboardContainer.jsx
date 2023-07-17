2e25ec5bd1f965adc8e35de7b9f80687f5aa0bbc
zeeshan-shani <muhammadzeeshan37104@gmail.com>
2023-07-17 12:38:32 +0500

    removed store from dashboard page

--- src/components/layout/dashboard_container/DashboardContainer.jsx
+++ src/components/layout/dashboard_container/DashboardContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./dashboard-container.css";
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
@@ -6,12 +6,10 @@ import Footer from "../footer/Footer";
 
 function DashboardContainer() {
   return (
-    <div
-      className={`w-screen`}
-    >
+    <div className={`w-screen`}>
       <Header />
-      <div className='layout-mask'></div>
-      <div className=''>
+      <div className="layout-mask"></div>
+      <div className="">
         <Outlet />
       </div>
       <Footer />
