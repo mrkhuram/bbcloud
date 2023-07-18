23dfdc823a204df46fa7e268a11af527d1532b41
niaziiii <muhabatkhanx@gmail.com>
2023-07-18 23:59:55 +0500

    added store overview page

--- src/pages/store/index.jsx
+++ src/pages/store/index.jsx
@@ -1,42 +1,37 @@
 import React from "react";
 import { dataStore } from "../../assets/data/store";
+import PageHeading from "../../components/PageHeading";
+import { Link } from "react-router-dom";
 
 function Store() {
   return (
-    <div
-      className="grid grid-cols-4 text-center text-black m-10"
-      style={{
-        display: "grid",
-        justifyContent: "center",
-        alignContent: "center",
-      }}
-      onClick={() => {
-        console.log("-------------->Display");
-      }}
-    >
-      {dataStore.map((data, index) => (
-        <a href={`/store/${data.ref}`}>
-          <div
-            key={index}
-            className="relative overflow-hidden"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              textAlign: "center",
-            }}
-          >
-            <img
-              src={data.image}
-              alt={`Clothing ${index + 1}`}
-              style={{ maxWidth: "60%", height: "auto" }}
-              className="transition-all transform-gpu hover:scale-105 hover:duration-300"
-            />
-            <p>{data.name}</p>
-            <p>{data.price}</p>
-          </div>
-        </a>
-      ))}
+    <div className="py-6 mt-20">
+      <PageHeading heading={"Store"} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-center text-black m-10">
+        {dataStore.map((data, index) => (
+          <Link to={`/store/${data.ref}`} key={index}>
+            <div
+              key={index}
+              className="relative overflow-hidden"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              <img
+                src={data.image}
+                alt={`Clothing ${index + 1}`}
+                style={{ maxWidth: "60%", height: "auto" }}
+                className="transition-all transform-gpu hover:scale-105 hover:duration-300"
+              />
+              <p>{data.name}</p>
+              <p>{data.price}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
