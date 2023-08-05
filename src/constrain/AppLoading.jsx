5c1741b88bb00e990c061f7e9d28838d860e2a8a
niaziiii <muhabatkhanx@gmail.com>
2023-08-05 12:54:47 +0000

    added loading spiner

--- src/constrain/AppLoading.jsx
+++ src/constrain/AppLoading.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import AppNotification from "./AppNotification";
+import { BeatLoader } from "react-spinners";
 
 const AppLoading = ({ notification, close }) => {
   return (
     <div>
-      <div className="h-[70vh] flex items-center justify-center">
-        <h2>loading...</h2>
+      <div className="h-[70vh] flex items-center justify-center bg-[#aeaeae]">
+        <BeatLoader
+          color={"#dbb52f"}
+          loading={true}
+          size={25}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+
         {notification && (
           <AppNotification message={notification} close={close} />
         )}
